1614cb9b4aa4295e4495b0af0d64a09234370452
Fabian Kramm <fab.kramm@googlemail.com>
2022-08-26 15:44:55 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/loft-sh/devspace-vscode-example
+
+go 1.18
--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package devspace_vscode_example
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func main() {
+	fmt.Println("Started server on :9000")
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9000", nil)
+}
